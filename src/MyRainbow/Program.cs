47ea8da66c81e13b44e828964d2a55de426127e9
ChaosEngine <andrzej.pauli@gmail.com>
2016-12-02 23:39:46 +0100

    1st use of hash (MD5)

--- src/MyRainbow/Program.cs
+++ src/MyRainbow/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,14 +12,17 @@ namespace MyRainbow
     {
         public static void Main(string[] args)
         {
-            MyCartesian cart = new MyCartesian(10);
+            MyCartesian cart = new MyCartesian(3, "abcdefg");
 
             Console.WriteLine($"Hello World! AlphabetPower  = {cart.AlphabetPower}");
 
             var table_of_table_of_chars = cart.Generate();
+            var hasher = MD5.Create();
             foreach (var chars_table in table_of_table_of_chars)
             {
-                Console.WriteLine(string.Concat(chars_table));
+                var str = string.Concat(chars_table);
+                var hash = BitConverter.ToString(hasher.ComputeHash(Encoding.UTF8.GetBytes(str))).Replace("-","").ToLowerInvariant();
+                Console.WriteLine($"MD5({str})= {hash}");
 
                 if (Console.KeyAvailable)
                     break;
