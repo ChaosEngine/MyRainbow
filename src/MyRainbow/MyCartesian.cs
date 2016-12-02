be014aaec8f08de5d79285d045e26f644570664d
chaos <chaos@WIN10VM>
2016-12-02 23:22:09 +0100

    Initial starting commit. Loosesly based ideas ATM

---
+++ src/MyRainbow/MyCartesian.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MyRainbow
+{
+    class MyCartesian
+    {
+        private const string _longAlphabet = @"abcdefghijklmnopqrstuvwxyz0123456789";
+        private const string _smallAlphabet = @"abcd";
+
+        private string _alphabet = null;
+
+        public int Length { get; private set; }
+
+        public int AlphabetPower { get { return _alphabet.Length; } }
+
+        public MyCartesian(int length = 4, string alphabet = null)
+        {
+            Length = length;
+
+            if (!string.IsNullOrEmpty(alphabet))
+                _alphabet = alphabet;
+            else
+                _alphabet = _smallAlphabet;
+        }
+
+        public IEnumerable<IEnumerable<char>> Generate()
+        {
+            var input = new string[Length];
+            for (int i = 0; i < Length; i++)
+                input[i] = _alphabet;
+
+            var result = input.CartesianProduct();
+
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// http://codereview.stackexchange.com/a/140435
+    /// </summary>
+    static class CartesianProducts
+    {
+        public static IEnumerable<IEnumerable<T>> CartesianProduct<T>(this IEnumerable<IEnumerable<T>> sources) =>
+            sources.AsParallel().Skip(1).Any() ?
+                sources.AsParallel().Skip(1).CartesianProduct().SelectMany(cp => sources.First().AsParallel().Select(s => new[] { s }.Concat(cp))) :
+                sources.First().Select(c => new[] { c });
+    }
+}
---
+++ src/MyRainbow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRainbow
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyCartesian cart = new MyCartesian(10);
+
+            Console.WriteLine($"Hello World! AlphabetPower  = {cart.AlphabetPower}");
+
+            var table_of_table_of_chars = cart.Generate();
+            foreach (var chars_table in table_of_table_of_chars)
+            {
+                Console.WriteLine(string.Concat(chars_table));
+
+                if (Console.KeyAvailable)
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
