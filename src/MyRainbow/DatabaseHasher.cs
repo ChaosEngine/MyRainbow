3feabf3aef3bf33d1066c0a804adc1cbf96c310e
ChaosEngine <andrzej.pauli@gmail.com>
2016-12-14 19:39:53 +0100

    refactoriztion to worker class

--- src/MyRainbow/DatabaseHasher.cs
+++ src/MyRainbow/DatabaseHasher.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace MyRainbow
 {
@@ -60,6 +64,131 @@ namespace MyRainbow
 			{
 				cmd.CommandType = CommandType.Text;
 				cmd.ExecuteNonQuery();
+			}
+		}
+
+		internal void Generate(IEnumerable<IEnumerable<char>> tableOfTableOfChars, MD5 hasherMD5, SHA256 hasherSHA256,
+			Func<string, string, string, long, long, bool> shouldBreakFun, Stopwatch stopwatch = null, int batchInsertCount = 200, int batchTransactionCommitCount = 20000)
+		{
+			string last_key_entry = GetLastKeyEntry();
+
+			double? nextPause = null;
+			if (stopwatch != null)
+			{
+				stopwatch.Start();
+				nextPause = stopwatch.Elapsed.TotalMilliseconds + 1000;//next check after 1sec
+			}
+			long counter = 0, last_pause_counter = 0, tps = 0;
+			var tran = Conn.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted);
+			SqlCommand cmd = new SqlCommand("", Conn, tran);
+			cmd.CommandType = System.Data.CommandType.Text;
+			int param_counter = 0;
+			foreach (var chars_table in tableOfTableOfChars)
+			{
+				var key = string.Concat(chars_table);
+				if (!string.IsNullOrEmpty(last_key_entry) && last_key_entry.CompareTo(key) >= 0) continue;
+				var hashMD5 = BitConverter.ToString(hasherMD5.ComputeHash(Encoding.UTF8.GetBytes(key))).Replace("-", "").ToLowerInvariant();
+				var hashSHA256 = BitConverter.ToString(hasherSHA256.ComputeHash(Encoding.UTF8.GetBytes(key))).Replace("-", "").ToLowerInvariant();
+
+				//dbase.Insert(value, hash);
+				cmd.CommandText += $"insert into hashes([key], hashMD5, hashSHA256)" +
+					$"values(@key{param_counter}, @hashMD5{param_counter}, @hashSHA256{param_counter});{Environment.NewLine}";
+				SqlParameter param;
+				if (cmd.Parameters.Contains($"@key{param_counter}"))
+				{
+					param = cmd.Parameters[$"@key{param_counter}"];
+					param.Value = key;
+				}
+				else
+				{
+					param = new SqlParameter($"@key{param_counter}", System.Data.SqlDbType.VarChar, 20);
+					param.Value = key;
+					cmd.Parameters.Add(param);
+				}
+
+				if (cmd.Parameters.Contains($"@hashMD5{param_counter}"))
+				{
+					param = cmd.Parameters[$"@hashMD5{param_counter}"];
+					param.Value = hashMD5;
+				}
+				else
+				{
+					param = new SqlParameter($"@hashMD5{param_counter}", System.Data.SqlDbType.Char, 32);
+					param.Value = hashMD5;
+					cmd.Parameters.Add(param);
+				}
+
+				if (cmd.Parameters.Contains($"@hashSHA256{param_counter}"))
+				{
+					param = cmd.Parameters[$"@hashSHA256{param_counter}"];
+					param.Value = hashSHA256;
+				}
+				else
+				{
+					param = new SqlParameter($"@hashSHA256{param_counter}", System.Data.SqlDbType.Char, 64);
+					param.Value = hashSHA256;
+					cmd.Parameters.Add(param);
+				}
+
+				param_counter++;
+
+				if (counter % batchInsertCount == 0)
+				{
+					//cmd.Prepare();
+					cmd.ExecuteNonQuery();
+					cmd.Dispose();
+					cmd.CommandText = "";
+					cmd.Connection = Conn;
+					cmd.Transaction = tran;
+					param_counter = 0;
+				}
+
+				if (counter % batchTransactionCommitCount == 0)
+				{
+					if (cmd != null && !string.IsNullOrEmpty(cmd.CommandText))
+					{
+						//cmd.Prepare();
+						cmd.ExecuteNonQuery();
+						cmd.Dispose();
+						cmd.CommandText = "";
+						cmd.Connection = Conn;
+						cmd.Transaction = tran;
+						param_counter = 0;
+					}
+					tran.Commit(); tran.Dispose();
+					tran = null;
+
+					//Console.WriteLine($"MD5({key})={hashMD5},SHA256({key})={hashSHA256},counter={counter},tps={tps}");
+					//if (Console.KeyAvailable)
+					//	break;
+					if (shouldBreakFun(key, hashMD5, hashSHA256, counter, tps))
+						break;
+
+					cmd.Transaction = tran = Conn.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted);
+				}
+
+				if (stopwatch != null && stopwatch.Elapsed.TotalMilliseconds >= nextPause)
+				{
+					if (last_pause_counter > 0)
+					{
+						tps = counter - last_pause_counter;
+						nextPause = stopwatch.Elapsed.TotalMilliseconds + 1000;
+					}
+					last_pause_counter = counter;
+				}
+
+				counter++;
+			}
+
+			if (cmd != null && !string.IsNullOrEmpty(cmd.CommandText))
+			{
+				//cmd.Prepare();
+				cmd.ExecuteNonQuery();
+				cmd.Dispose();
+			}
+			if (tran != null)
+			{
+				tran.Commit(); tran.Dispose();
 			}
 		}
 
--- src/MyRainbow/Program.cs
+++ src/MyRainbow/Program.cs
@@ -45,120 +45,16 @@ namespace MyRainbow
 				{
 					dbase.EnsureExist();
 					//dbase.Purge();
-					string last_key_entry = dbase.GetLastKeyEntry();
 
-					stopwatch.Start();
-					double nextPause = stopwatch.Elapsed.TotalMilliseconds + 1000;//next check after 1sec
-					long counter = 0, last_pause_counter = 0, tps = 0;
-					var tran = conn.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted);
-					SqlCommand cmd = new SqlCommand("", conn, tran);
-					cmd.CommandType = System.Data.CommandType.Text;
-					int param_counter = 0;
-					foreach (var chars_table in table_of_table_of_chars)
-					{
-						var key = string.Concat(chars_table);
-						if (!string.IsNullOrEmpty(last_key_entry) && last_key_entry.CompareTo(key) >= 0) continue;
-						var hashMD5 = BitConverter.ToString(hasherMD5.ComputeHash(Encoding.UTF8.GetBytes(key))).Replace("-", "").ToLowerInvariant();
-						var hashSHA256 = BitConverter.ToString(hasherSHA256.ComputeHash(Encoding.UTF8.GetBytes(key))).Replace("-", "").ToLowerInvariant();
-
-						//dbase.Insert(value, hash);
-						cmd.CommandText += $"insert into hashes([key], hashMD5, hashSHA256)" +
-							$"values(@key{param_counter}, @hashMD5{param_counter}, @hashSHA256{param_counter});{Environment.NewLine}";
-						SqlParameter param;
-						if (cmd.Parameters.Contains($"@key{param_counter}"))
+					dbase.Generate(table_of_table_of_chars, hasherMD5, hasherSHA256,
+						(key, hashMD5, hashSHA256, counter, tps) =>
 						{
-							param = cmd.Parameters[$"@key{param_counter}"];
-							param.Value = key;
-						}
-						else
-						{
-							param = new SqlParameter($"@key{param_counter}", System.Data.SqlDbType.VarChar, 20);
-							param.Value = key;
-							cmd.Parameters.Add(param);
-						}
-
-						if (cmd.Parameters.Contains($"@hashMD5{param_counter}"))
-						{
-							param = cmd.Parameters[$"@hashMD5{param_counter}"];
-							param.Value = hashMD5;
-						}
-						else
-						{
-							param = new SqlParameter($"@hashMD5{param_counter}", System.Data.SqlDbType.Char, 32);
-							param.Value = hashMD5;
-							cmd.Parameters.Add(param);
-						}
-
-						if (cmd.Parameters.Contains($"@hashSHA256{param_counter}"))
-						{
-							param = cmd.Parameters[$"@hashSHA256{param_counter}"];
-							param.Value = hashSHA256;
-						}
-						else
-						{
-							param = new SqlParameter($"@hashSHA256{param_counter}", System.Data.SqlDbType.Char, 64);
-							param.Value = hashSHA256;
-							cmd.Parameters.Add(param);
-						}
-
-						param_counter++;
-
-						if (counter % 200 == 0)
-						{
-							cmd.Prepare();
-							cmd.ExecuteNonQuery();
-							cmd.Dispose();
-							cmd.CommandText = "";
-							cmd.Connection = conn;
-							cmd.Transaction = tran;
-							param_counter = 0;
-						}
-
-						if (counter % 20000 == 0)
-						{
-							if (cmd != null && !string.IsNullOrEmpty(cmd.CommandText))
-							{
-								cmd.Prepare();
-								cmd.ExecuteNonQuery();
-								cmd.Dispose();
-								cmd.CommandText = "";
-								cmd.Connection = conn;
-								cmd.Transaction = tran;
-								param_counter = 0;
-							}
-							tran.Commit(); tran.Dispose();
-							tran = null;
-
 							Console.WriteLine($"MD5({key})={hashMD5},SHA256({key})={hashSHA256},counter={counter},tps={tps}");
 							if (Console.KeyAvailable)
-								break;
-
-							cmd.Transaction = tran = conn.BeginTransaction(System.Data.IsolationLevel.ReadUncommitted);
-						}
-
-						if (stopwatch.Elapsed.TotalMilliseconds >= nextPause)
-						{
-							if (last_pause_counter > 0)
-							{
-								tps = counter - last_pause_counter;
-								nextPause = stopwatch.Elapsed.TotalMilliseconds + 1000;
-							}
-							last_pause_counter = counter;
-						}
-
-						counter++;
-					}
-
-					if (cmd != null && !string.IsNullOrEmpty(cmd.CommandText))
-					{
-						cmd.Prepare();
-						cmd.ExecuteNonQuery();
-						cmd.Dispose();
-					}
-					if (tran != null)
-					{
-						tran.Commit(); tran.Dispose();
-					}
+								return true;
+							return false;
+						}, stopwatch);
+					
 					stopwatch.Stop();
 				}
 			}
