70a580a8552100d7a55b20d41b33ac8badea4661
ChaosEngine <andrzej.pauli@gmail.com>
2017-06-28 22:58:49 +0200

    updated docker build image to latest

--- src/MyRainbow/prepareOutputForDockerImage.sh
+++ src/MyRainbow/prepareOutputForDockerImage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #
-docker run -it --rm -v $(pwd):/app --workdir /app microsoft/aspnetcore-build:1.1.0-projectjson bash -c "dotnet restore && dotnet publish -c Release"
+docker run -it --rm -v $(pwd):/app --workdir /app microsoft/aspnetcore-build:latest bash -c "dotnet restore && dotnet publish -c Release"
